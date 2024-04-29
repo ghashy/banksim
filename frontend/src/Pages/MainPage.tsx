ae3ad7ca1fef02855eda8e39f54b9d0ca7f2d10c
olegus199 <okremnyov@yandex.ru>
2024-04-29 17:09:15 +0300

    Add 2 more action buttons

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,17 +1,32 @@
+import { useSelector } from "react-redux";
 import AccountTable from "../Components/AccountTable";
 import ActionButton from "../UI/ActionButton";
 import styles from "./MainPage.module.scss";
 import { FC } from "react";
+import { RootState } from "../state/store";
 
 const MainPage: FC = () => {
+  const checked_items = useSelector<RootState, string[]>(
+    (state) => state.checked_items.items
+  );
+
   return (
     <section className={styles.main_page}>
       <div className={styles.content}>
         <div className={styles.header_container}>
           <h2 className={styles.h2}>Accounts</h2>
           <div className={styles.action_buttons}>
-            <ActionButton kind="new_transaction" />
-            <ActionButton kind="new_account" />
+            {checked_items.length === 0 ? (
+              <>
+                <ActionButton kind="new_transaction" />
+                <ActionButton kind="new_account" />
+              </>
+            ) : (
+              <>
+                <ActionButton kind="open_credit" />
+                <ActionButton kind="delete_account" />
+              </>
+            )}
           </div>
         </div>
         <AccountTable />
