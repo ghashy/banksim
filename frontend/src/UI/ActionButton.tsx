df85ded07c15f95397788531cbfd489a0130927f
olegus199 <okremnyov@yandex.ru>
2024-04-29 17:09:50 +0300

    Add plural for delete account

--- frontend/src/UI/ActionButton.tsx
+++ frontend/src/UI/ActionButton.tsx
@@ -3,12 +3,19 @@ import { FC, useEffect, useState } from "react";
 import { ActionButtonKind } from "../types";
 import { AiOutlineDollarCircle } from "react-icons/ai";
 import { LuPlusCircle } from "react-icons/lu";
+import { IoTrash } from "react-icons/io5";
+import { TbCreditCardPay } from "react-icons/tb";
+import { useSelector } from "react-redux";
+import { RootState } from "../state/store";
 
 interface ActionButtonProps {
   kind: ActionButtonKind;
 }
 
 const ActionButton: FC<ActionButtonProps> = ({ kind }) => {
+  const checked_items = useSelector<RootState, string[]>(
+    (state) => state.checked_items.items
+  );
   const [button_name, set_button_name] = useState("");
 
   useEffect(() => {
@@ -26,7 +33,17 @@ const ActionButton: FC<ActionButtonProps> = ({ kind }) => {
         set_button_name("open credit");
         break;
     }
-  }, []);
+  }, [kind]);
+
+  useEffect(() => {
+    if (kind === "delete_account") {
+      if (checked_items.length <= 1) {
+        set_button_name("delete account");
+      } else {
+        set_button_name("delete accounts");
+      }
+    }
+  }, [checked_items.length]);
 
   return (
     <div className={styles.action_button}>
@@ -37,6 +54,10 @@ const ActionButton: FC<ActionButtonProps> = ({ kind }) => {
       {kind === "new_transaction" && (
         <AiOutlineDollarCircle className={styles.button_icon} />
       )}
+      {kind === "open_credit" && (
+        <TbCreditCardPay className={styles.button_icon} />
+      )}
+      {kind === "delete_account" && <IoTrash className={styles.button_icon} />}
     </div>
   );
 };
