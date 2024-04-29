1302061554f789d60dc7064f2f57232b59e02424
olegus199 <okremnyov@yandex.ru>
2024-04-29 13:15:03 +0300

    Add types

---
+++ frontend/src/types.ts
@@ -0,0 +1,14 @@
+export type ActionButtonKind =
+  | "new_account"
+  | "new_transaction"
+  | "delete_account"
+  | "open_credit";
+
+export interface IAccount {
+  card_number: string;
+  balance: number;
+  transactions: number;
+  exists: boolean;
+  tokens: string[];
+  username: string;
+}
