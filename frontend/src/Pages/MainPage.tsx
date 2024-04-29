458fb8e5e03856cf629091f9f81b2a7868fe106c
olegus199 <okremnyov@yandex.ru>
2024-04-29 13:17:12 +0300

    Import Accounts table, correct styles

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,10 +1,21 @@
+import AccountTable from "../Components/AccountTable";
+import ActionButton from "../UI/ActionButton";
 import styles from "./MainPage.module.scss";
 import { FC } from "react";
 
 const MainPage: FC = () => {
   return (
     <section className={styles.main_page}>
-      <div className={styles.content}>Main page</div>
+      <div className={styles.content}>
+        <div className={styles.header_container}>
+          <h2 className={styles.h2}>Accounts</h2>
+          <div className={styles.action_buttons}>
+            <ActionButton kind="new_transaction" />
+            <ActionButton kind="new_account" />
+          </div>
+        </div>
+        <AccountTable />
+      </div>
     </section>
   );
 };
