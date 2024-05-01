a77eb673cce8d824cdc019597b4cc68a443ab2da
olegus199 <okremnyov@yandex.ru>
2024-05-01 10:55:14 +0300

    Delete "unselect all" button

--- frontend/src/Components/AccountTable.tsx
+++ frontend/src/Components/AccountTable.tsx
@@ -3,16 +3,8 @@ import { FC } from "react";
 import TableHeader from "./TableHeader";
 import TableRow from "./TableRow";
 import { accounts } from "../mock_data";
-import { RootState } from "../state/store";
-import { useDispatch, useSelector } from "react-redux";
-import { reset_checked_itmes } from "../state/checked_items_slice";
 
 const AccountTable: FC = () => {
-  const checked_items = useSelector<RootState, string[]>(
-    (state) => state.checked_items.items
-  );
-  const dispatch = useDispatch();
-
   return (
     <div className={styles.account_table}>
       <TableHeader />
@@ -24,15 +16,6 @@ const AccountTable: FC = () => {
             key={idx}
           />
         ))}
-      </div>
-      <div
-        className={styles.unselect_all}
-        style={{
-          visibility: `${checked_items.length !== 0 ? "visible" : "hidden"}`,
-        }}
-        onClick={() => dispatch(reset_checked_itmes())}
-      >
-        unselect all
       </div>
     </div>
   );
