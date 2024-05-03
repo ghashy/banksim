0cdec9b9295806d75fb41669f994c622af76de54
olegus199 <okremnyov@yandex.ru>
2024-05-03 08:43:16 +0300

    Update types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -8,8 +8,27 @@ export type ActionKind =
 export interface IAccount {
   card_number: string;
   balance: number;
-  transactions: number;
+  transactions: ITransactions[];
   exists: boolean;
   tokens: string[];
   username: string;
+}
+
+interface ITransactions {
+  amount: number;
+  datetime: string;
+  recipient: IInterlocutor;
+  sender: IInterlocutor;
+}
+
+interface IInterlocutor {
+  card_number: string;
+  is_existing: boolean;
+  username: string;
+}
+
+export interface IStoreInfo {
+  card: string;
+  balance: string;
+  emission: string;
 }
