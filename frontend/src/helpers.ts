e10f83c1076a9c7d569cacbc0f61b5ee099914b4
olegus199 <okremnyov@yandex.ru>
2024-05-03 08:43:24 +0300

    Update helpers

--- frontend/src/helpers.ts
+++ frontend/src/helpers.ts
@@ -1,3 +1,7 @@
 export function format_price(price: number): string {
   return price.toLocaleString().replace(/,/g, "_");
 }
+
+export const wait = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
