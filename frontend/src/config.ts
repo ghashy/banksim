c6944087c1fce291fd29f7b0ad550faef5239fee
olegus199 <okremnyov@yandex.ru>
2024-05-04 09:00:34 +0300

    Add global config vars

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,3 +3,8 @@ export const API_URL = env.VITE_REACT_APP_API_URL || "http://localhost:15100";
 
 export const MAX_RETRIES = 7;
 export const RETRY_DELAY_MS = 1000;
+
+const username = "ghashy";
+const password = "terminalpassword";
+const token = btoa(`${username}:${password}`);
+export const AUTH_HEADER = `Basic ${token}`;
