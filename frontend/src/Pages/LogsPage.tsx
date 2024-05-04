2e4dda54614a1b11a754feee744b282aad445181
olegus199 <okremnyov@yandex.ru>
2024-05-04 18:50:06 +0300

    Fully finctional logs page

--- frontend/src/Pages/LogsPage.tsx
+++ frontend/src/Pages/LogsPage.tsx
@@ -1,20 +1,23 @@
 import Ansi from "ansi-to-react";
 import styles from "./LogsPage.module.scss";
 import { FC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../state/store";
 
 const LogsPage: FC = () => {
+  const logs = useSelector<RootState, string[]>((state) => state.logs.logs);
   return (
     <section className={styles.logs_page}>
       <div className={styles.content}>
-        <Ansi>{`1. Error log with timestamp:
-\u001b[31m[ERROR] \u001b[37m2022-04-01 13:45:22 \u001b[0mAn unexpected error occurred: Connection timed out`}</Ansi>
-        <br />
-        <Ansi>{`2. Warning log with detailed message:
-\u001b[33m[WARNING] \u001b[37m[3145] \u001b[0mPossible data inconsistency detected in user records`}</Ansi>
-        <br />
-        <Ansi>{`3. Info log with request details:
-\u001b[36m[INFO] \u001b[37mGET /api/users/1234 \u001b[0mRequest successfully processed in 50ms`}</Ansi>
-        <br />
+        <h2 className={styles.h2}>Logs</h2>
+        {logs.map((log, idx) => {
+          return (
+            <div key={idx}>
+              <Ansi>{log}</Ansi>
+              <br />
+            </div>
+          );
+        })}
       </div>
     </section>
   );
